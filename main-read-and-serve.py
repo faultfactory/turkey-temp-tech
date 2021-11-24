f800922008a87e4d2e5c60f9045c29b964597c31
faultfactory <32714474+faultfactory@users.noreply.github.com>
2021-11-24 14:58:55 -0500

    adding multple graphs

--- main-read-and-serve.py
+++ main-read-and-serve.py
@@ -8,28 +8,97 @@ import random
 import plotly.graph_objs as go
 from collections import deque
 
-X = deque(maxlen = 20)
+X = deque(maxlen = 600)
 X.append(1)
 
-Y = deque(maxlen = 20)
+Y = deque(maxlen = 600)
 Y.append(1)
+
+lowSpeed_thigh = deque(maxlen = 600)
+lowSpeed_breast = deque(maxlen = 600)
+
+highSpeed_thigh = deque(maxlen = 600)
+highSpeed_breast = deque(maxlen = 600)
+
+lowSpeedIntervalMS = 5000
+highSpeedIntervalMS = 200
+
 
 app = dash.Dash(__name__)
 
+# app.layout = html.Div(
+# 	[	html.Label('Thigh'),
+# 		dcc.Graph(id = 'thigh-graph', animate = True),
+# 		dcc.Interval(
+# 			id = 'slow-update',
+# 			interval = 1000,
+# 			n_intervals = 0
+# 		)
+# 	],
+# 	[ 	html.Label('Breast'),
+# 		dcc.Graph(id = 'breast-graph', animate = True),
+		# dcc.Interval(
+		# 	id = 'fast-update',
+		# 	interval = 1000,
+		# 	n_intervals = 0
+		# )
+# 	]
+# )
+
 app.layout = html.Div(
-	[
-		dcc.Graph(id = 'live-graph', animate = True),
-		dcc.Interval(
-			id = 'graph-update',
+	[html.H1(children='Turkey Vision 3000'),
+	html.H3(children="FriendsgivingTech Advanced Development Group"),
+    # All elements from the top of the page
+    html.Div([
+        html.Div([
+            html.H1(children='Thigh Temperature - High Speed Trace'),
+
+            html.Div(children='''
+                Dash: A web application framework for Python.
+            '''),
+
+            dcc.Graph(
+                id='graph1',
+				animate = True
+            ),
+			dcc.Interval(
+			id = 'fast-update',
 			interval = 1000,
 			n_intervals = 0
-		),
-	]
-)
+		)  
+        ], className='six columns'),
+        html.Div([
+            html.H1(children='Hello Dash'),
+
+            html.Div(children='''
+                Dash: A web application framework for Python.
+            '''),
+
+            dcc.Graph(
+                id='graph2',
+				animate = True
+            ),  
+        ], className='six columns'),
+    ], className='row'),
+    # New Div for all elements in the new 'row' of the page
+    html.Div([
+        html.H1(children='Hello Dash'),
+
+        html.Div(children='''
+            Dash: A web application framework for Python.
+        '''),
+
+        dcc.Graph(
+            id='graph3',
+			animate = True
+        ),
+		  
+    ], className='row'),
+])
 
 @app.callback(
-	Output('live-graph', 'figure'),
-	[ Input('graph-update', 'n_intervals') ]
+	Output('thigh-graph', 'figure'),
+	[ Input('slow-update', 'n_intervals') ]
 )
 
 def update_graph_scatter(n):
